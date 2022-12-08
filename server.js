82e2dcc5f7d7c8c744e1fd5c16d2e169d5cf3f04
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2022-12-07 17:25:52 -0800

    Added to server

--- server.js
+++ server.js
@@ -1,34 +1,37 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const db = require('./data/weather.json');
-
-// this is a singleton, meaning there can only one!
-const app = express(); // starts the framework, now we have to configure the object the framework returns.
+const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT|| 3001;
+const PORT = process.env.PORT || 3001;
 
 // specify a METHOD,  provide an endpoint for the URL
 app.get('/weather', (request, response, next) => {
-  // do something in response!
-  console.log(request.query, db);
-//   if (!request.query.name) {
-//     next('Please provide a valid city.'); // to handle an error on the server, pass any value into next.
-    
-//   } else {
-    // find our weather
-    // let weatherData = {};
-    let {searchQuery}=request.query;
+    // do something in response!
+    console.log(request.query, db);
+    //   if (!request.query.name) {
+    //     next('Please provide a valid city.'); // to handle an error on the server, pass any value into next.
+    let { searchQuery } = request.query;
     const cityWeather = db.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase())
-    response.send(cityWeather);
-    // }
-  });
+    let arrDays = cityWeather.data;
+    let forecastArr = arrDays.map(day => new Forecast(day))
+    response.send(forecastArr);
 
+
+});
+
+function Forecast(day) {
+    this.date = day.valid_date;
+    this.description = day.weather.description;
+}
+app.use;
 // catch all requests that don't match a METHOD or URL above.
 app.use('*', (request, response, next) => {
-  response.status(404).send('Invalid Request, route not found');
+    response.status(404).send('Invalid Request, route not found');
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
